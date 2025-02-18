f366e92d9496d28c67f54f7884b24f3b2f1c2caa
marshot9420 <124557537+marshot9420@users.noreply.github.com>
2025-02-19 08:38:31 +0900

    6. 서브메뉴 만들어보기와 classList 다루기

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
 const navbarToggler = document.querySelector(".navbar-toggler");
 const listGroup = document.querySelector(".list-group");
 
-function handleToggleNavbar() {}
+function handleToggleNavbar() {
+  if (listGroup.classList.contains("show")) {
+    listGroup.classList.remove("show");
+  } else {
+    listGroup.classList.add("show");
+  }
+}
+
+navbarToggler.addEventListener("click", handleToggleNavbar);
